5522db9ea31104b7aa5f229b385eaed4eaa16e77
Sushant <sushantnistane1@gmail.com>
2025-10-02 00:11:24 +0530

    fixed multer and file extension

--- backend_express/routes/user.routes.js
+++ backend_express/routes/user.routes.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const multer = require("multer");
+const path = require("path");
 const { signup, login } = require("../controllers/user.controller.js");
 
 const router = express.Router();
 
-const upload = multer({ dest: "uploads/" }); 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname); 
+  },
+});
+
+const upload = multer({ storage });
 
 router.post("/signup", upload.single("file"), signup);
 router.post("/login", login);
